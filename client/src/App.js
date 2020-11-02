14acbb5c4a3b98515334ef4d3910e49f8ccb1684
Matthew Everett <matthew@mjheverett.com>
2020-11-02 14:50:23 -0500

    Creating docker config files
    Co-authored-by: Dylan Cooper <dylancooper155@gmail.com>
    Co-authored-by: Harmony <harmonytrevena@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -3,18 +3,24 @@ import { Route, Switch } from 'react-router-dom';
 import './App.css';
 import ProtectedRoute from './auth/ProtectedRoute';
 import Home from './components/Home';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
-      <Switch>
-        <Route exact path="/">
-          <Home />
-        </Route>
-        <ProtectedRoute path="/profile">
+      <Navbar />
+      <div>
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <ProtectedRoute path="/profile">
 
-        </ProtectedRoute>
-      </Switch>
+          </ProtectedRoute>
+        </Switch>
+      </div>
+      <Footer />
     </div>
   );
 }
