0cbeae1188b08c91a09df28ca32669f55a2e0b5a
Dylan Cooper <dylancooper155@gmail.com>
2020-11-02 17:15:00 -0500

    added switch/icon for lightdarkmode

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import "./App.css";
+
 import ProtectedRoute from "./auth/ProtectedRoute";
 import Home from "./components/Home";
 import LightDarkToggle from "./components/LightDark/LightDarkToggle";
--- client/src/components/LightDark/LightDarkToggle.jsx
+++ client/src/components/LightDark/LightDarkToggle.jsx
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
 import LightDarkMode from "./LightDarkMode";
 import { ThemeProvider } from "styled-components";
+import { FormControlLabel, Switch, FormGroup } from '@material-ui/core';
+import NightsStayIcon from '@material-ui/icons/NightsStay';
 
 const LightDarkToggle = () => {
   const [theme, setTheme] = useState({ mode: "light" });
@@ -8,16 +10,16 @@ const LightDarkToggle = () => {
     <div>
       <ThemeProvider theme={theme}>
         <LightDarkMode />
+        
+        <FormGroup >
+        <FormControlLabel
+        control={<Switch style={{color: '#D33682'}} size="Normal" onChange={(e) =>
+        setTheme(
+          theme.mode === "dark" ? { mode: "light" } : { mode: "dark" }
+        )} />}
+        label={<NightsStayIcon fontSize='small'/>} /> 
 
-        <button
-          onClick={(e) =>
-            setTheme(
-              theme.mode === "dark" ? { mode: "light" } : { mode: "dark" }
-            )
-          }
-        >
-          Toggle
-        </button>
+        </FormGroup>
       </ThemeProvider>
     </div>
   );
