49230b5cd22455ba4b8935e70af0471600b82fe6
Harmony Trevena <harmonytrevena@gmail.com>
2020-11-02 17:20:26 -0500

    Added container to app

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -2,12 +2,20 @@ import React from 'react'
 import AuthenticationButton from './Auth0/AuthenticationButton';
 import SignupButton from './Auth0/SignUpButton';
 
+// Imports for Page Layout
+import { Container } from '@material-ui/core';
+
+// Styled components for all CSS
+
+
 const Home = () => {
     return (
         <>
+        <Container >
             <h1>Book Club App</h1>
             <AuthenticationButton />
             <SignupButton />
+        </Container>
         </>
     )
 };
