9a9980012e47fff4343305d3d1a7c5cd667d037a
Harmony Trevena <harmonytrevena@gmail.com>
2020-11-03 11:42:13 -0500

    Updated Navbar

--- client/src/components/Navbar/Navbar.jsx
+++ client/src/components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ const Navbar = () => {
     return (
         <div className="header">
             <div className="header-nav">
-                <h1 className="navbar-title">Book Club App</h1>
+                <h1 className="navbar-title">bookmode</h1>
             </div>
             <div className="header-nav">
                 <Link to="/" className="link">Home</Link>
