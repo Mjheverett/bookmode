3d8585348bc522b48dd2855ccd7eb1b804541f1e
Matthew Everett <matthew@mjheverett.com>
2020-11-05 12:02:25 -0500

    Updating express setup

--- service/app.js
+++ service/app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var cors = require('cors');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -10,6 +11,7 @@ var groupsRouter = require('./routes/groups');
 
 var app = express();
 
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
