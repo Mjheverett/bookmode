800fd5a7beb415d2ca07e37a17096cef48b7a0f9
Matthew Everett <matthew@mjheverett.com>
2020-11-05 12:26:01 -0500

    Updating docker configs

--- service/app.js
+++ service/app.js
@@ -1,15 +1,15 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
-var cors = require('cors');
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+const cors = require('cors');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var groupsRouter = require('./routes/groups');
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const groupsRouter = require('./routes/groups');
 
-var app = express();
+const app = express();
 
 app.use(cors());
 app.use(logger('dev'));
