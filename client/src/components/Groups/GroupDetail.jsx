41da911c9856bc73875477f55c26f182a1f2c094
Harmony Trevena <harmonytrevena@gmail.com>
2020-11-07 14:25:27 -0500

    Started styling the group page

--- client/src/components/Groups/GroupDetail.jsx
+++ client/src/components/Groups/GroupDetail.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React from 'react';
+import { Typography, GridListTile }  from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+    groupBar: {
+        background: '#52781e',
+        margin: theme.spacing(2),
+        padding: theme.spacing(2),
+    },
+}));
 
 const GroupDetail = (props) => {
+    const classes = useStyles();
     const { group } = props;
     console.log("props", props)
 
     return (
         <>
-            <h4>{group.groupName}</h4>
+            <br />
+                <GridListTile className={classes.groupBar} cellHeight={'auto'}>
+                    <Typography variant="h6" style={{color: '#fff'}}>{group.groupName}</Typography>
+                    <Typography style={{color: '#fff'}}>{group.groupDescription}</Typography>
+                </GridListTile>
+            <br />
         </>
     )
 }
